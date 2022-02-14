7b4eb34fd6787036f8ead6db1d9047c8bbc478a7
Conan0706 <s1f101903007@iniad.org>
2022-02-14 05:47:44 +0000

    カウントタイトルまで、空で送信すると数値消える

--- app.rb
+++ app.rb
@@ -4,7 +4,62 @@ require 'sinatra/reloader' if development?
 require './models.rb'
 
 before do
-  if Count.count == 0
+  if Count.count == 1
     Count.create(number: 0)
   end
 end
+
+get "/" do
+  @number = Count.first.number
+  @number2 = Count.find(2).number
+  erb :index
+end
+
+post "/plus" do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect "/"
+end
+
+post "/plus2" do
+  count = Count.find(2)
+  count.number = count.number + 1
+  count.save
+  redirect "/"
+end
+
+post "/minus" do
+  count = Count.first
+  count.number = count.number - 1
+  count.save
+  redirect "/"
+end
+
+post "/multi" do
+  count = Count.first
+  count.number = count.number * 2
+  count.save
+  redirect "/"
+end
+
+post "/division" do
+  count = Count.first
+  count.number = count.number / 2
+  count.save
+  redirect "/"
+end
+
+post "/clear" do
+  count = Count.first
+  count.number = 0
+  count.save
+  redirect "/"
+end
+
+post "/text" do
+  @text = params[:text]
+  erb :index
+  
+end
+
